6f9c4d5fe913cbc9f9b989b9ed72e919077a64bb
webrx <webrx@126.com>
2017-06-08 10:54:17 +0800

    编写了db.properties 文件实现读取

--- src/main/java/com/fz/User.java
+++ src/main/java/com/fz/User.java
@@ -1,7 +1,21 @@
 package com.fz;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Created by webrx on 2017-06-08.
  */
 public class User {
+    public static void main(String[] args) {
+        Properties pro = new Properties();
+        try {
+            pro.load(User.class.getClassLoader().getResourceAsStream("db.properties"));
+            System.out.println(pro.get("user"));
+            System.out.println(pro.get("password"));
+            System.out.println(pro.get("dbname"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/test/java/com/Hello.java
+++ src/test/java/com/Hello.java
@@ -1,7 +1,25 @@
 package com;
 
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Created by webrx on 2017-06-08.
  */
 public class Hello {
+
+    @Test
+    public void yy(){
+        Properties pro = new Properties();
+        try {
+            pro.load(Hello.class.getClassLoader().getResourceAsStream("db.properties"));
+            System.out.println(pro.get("user"));
+            System.out.println(pro.get("password"));
+            System.out.println(pro.get("dbname"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
